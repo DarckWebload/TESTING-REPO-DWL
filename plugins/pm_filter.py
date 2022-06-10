089ffe4c1b624e9941c97111d922465a9cbe1e0c
DarckWebload <106021000+DarckWebload@users.noreply.github.com>
2022-06-10 15:48:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -587,16 +587,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-    elif query.data == "newdata":
-        buttons = [[
-            InlineKeyboardButton('⬅️ 𝖡𝖺𝖼𝗄', callback_data='help')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.FILE_TXT,
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
     elif query.data == "json":
         buttons = [[ 
             InlineKeyboardButton('🚶 ʙᴀᴄᴋ', callback_data='help')
